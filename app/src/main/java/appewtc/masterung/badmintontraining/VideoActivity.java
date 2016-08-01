f6b799032dbb6198173e8d85d6484ff4ff74e088
masterUNG <phrombutr@gmail.com>
2016-08-01 16:33:11 +0700

    Back Video finish

--- app/src/main/java/appewtc/masterung/badmintontraining/VideoActivity.java
+++ app/src/main/java/appewtc/masterung/badmintontraining/VideoActivity.java
@@ -2,6 +2,8 @@ package appewtc.masterung.badmintontraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-    }
-}
+
+        //Active Back
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }   // Main Method
+
+}   // Main Class
