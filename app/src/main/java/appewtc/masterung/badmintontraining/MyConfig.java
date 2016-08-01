4e6f80eb5fe8ee00e3ada021336419c1cc612073
masterUNG <phrombutr@gmail.com>
2016-08-01 14:29:44 +0700

    Design UI SplashScreen

---
+++ app/src/main/java/appewtc/masterung/badmintontraining/MyConfig.java
@@ -0,0 +1,24 @@
+package appewtc.masterung.badmintontraining;
+
+/**
+ * Created by masterUNG on 8/1/2016 AD.
+ */
+public class MyConfig {
+
+    //Explicit
+    private String team1String = "โดเรมอน";
+    private String team2String = "โนบิตะ";
+    private int logoAnInt = R.drawable.blue;
+
+    public String getTeam1String() {
+        return team1String;
+    }
+
+    public String getTeam2String() {
+        return team2String;
+    }
+
+    public int getLogoAnInt() {
+        return logoAnInt;
+    }
+}   // Main Class
--- app/src/main/java/appewtc/masterung/badmintontraining/SplashScreen.java
+++ app/src/main/java/appewtc/masterung/badmintontraining/SplashScreen.java
@@ -2,12 +2,31 @@ package appewtc.masterung.badmintontraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    //Explicit
+    private ImageView imageView;
+    private TextView team1TextView, team2TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        //Bind Widget
+        imageView = (ImageView) findViewById(R.id.imageView);
+        team1TextView = (TextView) findViewById(R.id.textView3);
+        team2TextView = (TextView) findViewById(R.id.textView4);
+
+        //Show View
+        MyConfig myConfig = new MyConfig();
+        imageView.setImageResource(myConfig.getLogoAnInt());
+        team1TextView.setText(myConfig.getTeam1String());
+        team2TextView.setText(myConfig.getTeam2String());
+
+    }   // Main Method
+
+}   // Main Class
