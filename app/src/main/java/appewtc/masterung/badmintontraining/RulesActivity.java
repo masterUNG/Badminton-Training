78929a12b74b2887ac861c2481594f9835607216
masterUNG <phrombutr@gmail.com>
2016-08-01 16:04:18 +0700

    RulesActivity Success

--- app/src/main/java/appewtc/masterung/badmintontraining/RulesActivity.java
+++ app/src/main/java/appewtc/masterung/badmintontraining/RulesActivity.java
@@ -2,6 +2,8 @@ package appewtc.masterung.badmintontraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RulesActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class RulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
-    }
-}
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }   // Main Method
+}   // Main Class
