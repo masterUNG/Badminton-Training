de5bdf162fb54b3c1ad1d24ff5650b4bbe75892b
masterUNG <phrombutr@gmail.com>
2016-08-02 11:53:15 +0700

    Syn JSON finish

--- app/src/main/java/appewtc/masterung/badmintontraining/VideoActivity.java
+++ app/src/main/java/appewtc/masterung/badmintontraining/VideoActivity.java
@@ -1,16 +1,34 @@
 package appewtc.masterung.badmintontraining;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class VideoActivity extends AppCompatActivity {
+
+    // Explicit
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        //Create ListView
+        SynchronizeVideo synchronizeVideo = new SynchronizeVideo(this, listView);
+        synchronizeVideo.execute();
 
         //Active Back
         Button button = (Button) findViewById(R.id.button2);
@@ -22,5 +40,46 @@ public class VideoActivity extends AppCompatActivity {
         });
 
     }   // Main Method
+
+    //Create Inner Class
+    private class SynchronizeVideo extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private Context context;
+        private ListView myListView;
+        private static final String urlJSON = "http://swiftcodingthai.com/kan/get_video.php";
+
+        public SynchronizeVideo(Context context, ListView myListView) {
+            this.context = context;
+            this.myListView = myListView;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("kanV1", "JSON ==> " + s);
+
+        }   // onPost
+
+    }   // SynVideo Class
+
 
 }   // Main Class
