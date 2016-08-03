88e87cc4306e5e8b1be657a511b7ab5110d9ff0b
masterUNG <phrombutr@gmail.com>
2016-08-03 13:25:58 +0700

    Confirm Alert

--- app/src/main/java/appewtc/masterung/badmintontraining/MainHub.java
+++ app/src/main/java/appewtc/masterung/badmintontraining/MainHub.java
@@ -1,5 +1,7 @@
 package appewtc.masterung.badmintontraining;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -40,16 +42,16 @@ public class MainHub extends AppCompatActivity implements View.OnClickListener {
 
         switch (view.getId()) {
 
-            case R.id.imageView2:
+            case R.id.imageView2:   //to กฎกติกา
                 startActivity(new Intent(MainHub.this, RulesActivity.class));
                 break;
-            case R.id.imageView3:
+            case R.id.imageView3:   // to ดู วิดีโอ
                 startActivity(new Intent(MainHub.this, VideoActivity.class));
                 break;
-            case R.id.imageView4:
-                startActivity(new Intent(MainHub.this, CheckScore.class));
+            case R.id.imageView4:   // to นับคะแนน
+                confirmCountScore();
                 break;
-            case R.id.imageView5:
+            case R.id.imageView5:   // to แสดงแผนที่ สนาม
                 startActivity(new Intent(MainHub.this, MapsActivity.class));
                 break;
             case R.id.imageView6:
@@ -59,5 +61,29 @@ public class MainHub extends AppCompatActivity implements View.OnClickListener {
         }   // switch
 
     }   // onClick
+
+    private void confirmCountScore() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle("นับคะแนน");
+        builder.setMessage("ประเภทการแข่ง คู่ หรือ เดี่ยว");
+        builder.setNegativeButton("คู่", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("เดี่ยว", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }   // confirm
 
 }   // Main Class
